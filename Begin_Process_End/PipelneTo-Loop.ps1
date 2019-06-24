898bca25943eac1753d5e7c8eac0a8a3c4f10cba
D1\gauletga <gabriel.gaulet.ext@louisvuitton.com>
2019-06-24 23:23:59 +0200

    Begin Process End - Helper

---
+++ Begin_Process_End/PipelneTo-Loop.ps1
@@ -0,0 +1,55 @@
+﻿Function PipelineTo-Loop
+{
+	[CmdletBinding()]
+	Param
+	(
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true)]
+		[ValidateNotNullOrEmpty()]
+		[String]
+		$Source
+	)
+	
+	Begin
+	{
+		write-host "Begin Data"
+	}
+	Process
+	{
+		Try
+		{
+			If ($Source -eq "5")
+			{
+				Throw [system.exception]::new(("Non pas la source N°: $($Source)"))
+			}
+			Write-Host "Processing source N°: $($Source)"
+			
+		}
+		Catch
+		{
+			$_.Exception.Message
+		}
+	}
+	End
+	{
+		write-host "End Data"
+	}
+}
+
+1..10 | PipelineTo-Loop
+
+#Results 
+<#
+Begin Data
+Processing source N°: 1
+Processing source N°: 2
+Processing source N°: 3
+Processing source N°: 4
+Non pas la source N°: 5
+Processing source N°: 6
+Processing source N°: 7
+Processing source N°: 8
+Processing source N°: 9
+Processing source N°: 10
+End Data
+#>
---
+++ Begin_Process_End/README.md
@@ -0,0 +1,24 @@
+# Begin Process End
+
+**PipelineTo-Loop** - Run Begin - Process All - Run End 
+
+
+
+
+
+##Details 
+### PipelineTo-Loop
+**Call :** 1..10 | PipelineTo-Loop
+**Result :**
+Begin Data
+Processing source N°: 1
+Processing source N°: 2
+Processing source N°: 3
+Processing source N°: 4
+Non pas la source N°: 5
+Processing source N°: 6
+Processing source N°: 7
+Processing source N°: 8
+Processing source N°: 9
+Processing source N°: 10
+End Data
