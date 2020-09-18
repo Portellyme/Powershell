d84ee81874a795ee28e8a21eda911b0fa6478fe6
Portellyme <36563030+Portellyme@users.noreply.github.com>
2020-09-18 11:41:24 +0200

    Update AppendAllLines.ps1

--- Writer/AppendAllLines.ps1
+++ Writer/AppendAllLines.ps1
@@ -2,9 +2,16 @@
 $Utf8NoBomEncoding = [System.Text.UTF8Encoding]::new($false)
 
 
-$FileUtf8 = "C:\temp\utf8.txt"
-$FileUtf8NoBom  = "C:\temp\utf8NoBom.txt"
+$FileUtf8Name = "C:\temp\utf8.txt"
+$FileUtf8NameNoBom  = "C:\temp\utf8NoBom.txt"
 $Output = "Output"
+
+
+$FileUtf8 = [System.IO.File]::ReadAllLines(FileUtf8)
+$FileUtf8NoBom = [System.IO.File]::ReadAllLines($FileUtf8NameNoBom)
+
+
+
 
 #Create New file 
 [System.IO.File]::WriteAllLines([string]$FileUtf8, [string[]]$Output)
